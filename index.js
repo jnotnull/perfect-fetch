a90ee46caae7c962a7ce4d31b2b63aa0b8f7ed53
jnotnull <252210289@qq.com>
2017-03-21 11:32:48 +0800

    修改bug

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
+'use strict';
 var URLSearchParams = require('url-search-params');
 
-export function sendrequest(url, data, method ='post') {
 
-  let searchParams = new URLSearchParams();
-  if (data) {
-    for (let param in data) {
-      if (data[param] !== null) {
-        searchParams.append(param, JSON.stringify(data[param]));
-      }
+module.exports = {
+    sendrequest: function(url, data, method = 'post') {
+        let searchParams = new URLSearchParams();
+        if (data) {
+            for (let param in data) {
+                if (data[param] !== null) {
+                    searchParams.append(param, JSON.stringify(data[param]));
+                }
+            }
+        }
+
+        return fetch(url, {
+                "method": method,
+                "credentials": "include",
+                "headers": {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                "body": searchParams.toString()
+            })
+            .then(res => res.json())
+            .then(data => {
+                return data;
+            });
+    },
+    upload: function(url, file, method = 'post') {
+        var formData = new FormData()
+        formData.append('image', file);
+
+        return fetch(url, {
+                method: 'POST',
+                body: formData
+            })
+            .then(res => res.json())
+            .then(data => {
+                return data;
+            });
     }
-  }
-
-  return fetch(url, {
-          "method": method,
-          "credentials": "include",
-          "headers": {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          "body": searchParams.toString()
-      })
-      .then(res => res.json())
-      .then(data => {
-        return data;
-      });
-}
-
-export function upload(url, file, method ='post') {
-  var formData = new FormData()
-  formData.append('image', file);
-
-  return fetch(url, {
-          method:'POST',
-          body: formData
-        })
-        .then(res => res.json())
-        .then(data => {
-            return data;
-          });
 }
